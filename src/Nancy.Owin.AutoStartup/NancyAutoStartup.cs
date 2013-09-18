43c0c58f7d97bbfea6a57185491c35d1695850c0
Steven Robbins <ste.robbins@gmail.com>
2013-09-18 20:51:59 +0100

    Added AutoStartup project

---
+++ src/Nancy.Owin.AutoStartup/NancyAutoStartup.cs
@@ -0,0 +1,46 @@
+﻿namespace Nancy.Owin.AutoStartup
+{
+    using System.Collections.Generic;
+
+    using global::Owin;
+    using global::Owin.AutoStartup;
+
+    public class NancyAutoStartup : IAutoStartup
+    {
+        private IDictionary<string, object[]> defaultBuilderCommands;
+
+        public string Name
+        {
+            get
+            {
+                return "Nancy";
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                return "/";
+            }
+        }
+
+        public IDictionary<string, object[]> DefaultBuilderCommands
+        {
+            get
+            {
+                return this.defaultBuilderCommands;
+            }
+        }
+
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.UseNancy();
+        }
+
+        public NancyAutoStartup()
+        {
+            this.defaultBuilderCommands = new Dictionary<string, object[]> { { "UseNancy", new object[] { } } };
+        }
+    }
+}
